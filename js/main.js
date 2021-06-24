bc076ed72d2fede9014495546a14636ffecab5a7
sivertillya <volk.sivert@gmail.com>
2021-06-24 18:10:42 +0300

    FINISH 2.0, add commets code

--- js/main.js
+++ js/main.js
@@ -10,7 +10,10 @@ $(document).ready(function() {
         [0,0,0,0,0,1,1,1],
         [0,0,0,0,0,1,1,1],
     ]
-    // Css классы
+    // 1 = black player
+    // 2 = white player
+
+    // Css class
     GAME_CSS_CLASS = {
         black: 'black',
         white: 'white',
@@ -18,27 +21,27 @@ $(document).ready(function() {
         false_move: 'select_checker_false'
     }
 
-    // обьявляем глобальные переменные
+    // Global variable
     let turnIsActive = false;
-    let activeTeam = 2; // первые ходят белые
+    let activeTeam = 2; // First move
     let activeElementCoords = null;
     let selectElement;
-    const DIAGONAL_TURNS_POSSIBLE = false;
+    const DIAGONAL_MOVE_POSSIBLE = false; // Diagonal move possible
     let posBlack = [];
     let posWhite = [];
 
 
-    let movePlayer = document.querySelector('#player')
-    movePlayer.innerHTML = "Первыми ходят " + (activeTeam % 2 ? "черные" : "белые");
+    let info_game = document.querySelector('#info_game')
+    info_game.innerHTML = "Первыми ходят " + (activeTeam % 2 ? "черные" : "белые");
 
 
-    // Строим доску и фигуры на ней
+    // Create board and checker
     const grid = document.createElement('table');
     for(let i = 0; i < board.length; i++) {
         const tr = document.createElement('tr');
         for(let j = 0; j < board[i].length; j++) {
             const td = document.createElement('td');
-            // сохраняем позиции для проверки кто выйграл
+            // Save position and add class 
             if (board[i][j] == 1) {
                 td.classList.add(GAME_CSS_CLASS.black);
                 posBlack.push([i, j]);
@@ -55,65 +58,62 @@ $(document).ready(function() {
     };
     $("#game")[0].appendChild(grid);
     
-    //Обработчик нажатий
+    //Event click
     grid.addEventListener('click', ClickChecker);
     function ClickChecker(event) {               
         const target = event.target;
-        if (target.tagName == 'TD') { // проверям есть ли поле по корому нажали клеткой доски
+        if (target.tagName == 'TD') { // Check click
             const row = +target.dataset.row;
             const col = +target.dataset.col;
             const cellOccupied = board[row][col];
 
             if (cellOccupied == activeTeam || cellOccupied==0) {
-                if (!cellOccupied && !turnIsActive) return;
-                if (cellOccupied && turnIsActive) return doSomeAction(row, col);
-                if (cellOccupied && !turnIsActive) return moveChecker(target, row, col);
-                return FinishMove(target, row, col);
+                if ((!cellOccupied && !turnIsActive) || (cellOccupied && turnIsActive)) return;
+                if (cellOccupied && !turnIsActive) return StartMoveChecker(target, row, col); //First click
+                return FinishMoveChecker(target, row, col); // Second click
             }
         }
-        
-        
     };
-    // Начинаем перемещение
-    function moveChecker(element, row, col) {
+
+    // Start Move
+    function StartMoveChecker(element, row, col) {
         if (turnIsActive) return;
         turnIsActive = true;
         activeTeam = board[row][col]
         activeElementCoords = [row, col];
         selectElement = element;
-        ViewMove(element, row, col)
+        DisplayPossibleMove(row, col)
         board[row][col] = 0;
     }
-    // Заканчуем перемещение
-    function FinishMove(element, row, col) {
+
+    // Finish Move
+    function FinishMoveChecker(element, row, col) {
         if(checkTurnValidity(row, col)) {
             board[row][col] = activeTeam;
             selectElement.classList.remove(activeTeam % 2 ? GAME_CSS_CLASS.black : GAME_CSS_CLASS.white);
             element.classList.add(activeTeam % 2 ? GAME_CSS_CLASS.black : GAME_CSS_CLASS.white);
             document.querySelectorAll('table td').forEach(n => n.classList.remove(GAME_CSS_CLASS.false_move))
-            if (!Comparison_Of_Arrays([row, col], activeElementCoords))activeTeam = activeTeam % 2 ? 2 : 1; // проверяем был ли сдвиг
+            if (!Comparison_Of_Arrays([row, col], activeElementCoords)) activeTeam = activeTeam % 2 ? 2 : 1; // Check  move
             activeElementCoords = null;
             turnIsActive = false;
+            info_game.innerHTML = "Ходят " + (activeTeam % 2 ? "черные" : "белые");
             CheckEnd();
-            movePlayer.innerHTML = "Ходят " + (activeTeam % 2 ? "черные" : "белые");
         }
     }
-    //
-    function ViewMove(cuurentElement, row, col) {
+
+    // Display Possible Move
+    function DisplayPossibleMove(row, col) {
         for(let i = 0; i < board.length; i++) {
             for(let j = 0; j < board[i].length; j++) {
-                if(!checkTurnValidity(i, j)) {
-                    if (!Comparison_Of_Arrays([i, j], [row, col])) {
-                    
-                        let element = document.querySelector(`[data-row="${i}"][data-col="${j}"]`)
-                        element.classList.add(GAME_CSS_CLASS.false_move);
-                    }
+                if(!checkTurnValidity(i, j) && !Comparison_Of_Arrays([i, j], [row, col])) {
+                    let element = document.querySelector(`[data-row="${i}"][data-col="${j}"]`)
+                    element.classList.add(GAME_CSS_CLASS.false_move);
                 }
             }
         }
     }
     
-    // проверяем конец ли игры
+    // Checked End Game
     function CheckEnd() {
         let tempB = [];
         let tempW = [];
@@ -123,28 +123,31 @@ $(document).ready(function() {
                 if (board[i][j] == 2) tempW.push([i, j]);
             }
         }
-        if (Comparison_Of_Arrays_2(tempB, posWhite)) alert("win: Black")
-        if (Comparison_Of_Arrays_2(tempW, posBlack)) alert("win: White")
+        if (Comparison_Of_Arrays_2(tempB, posWhite)) DisplayWinPlayer("черные")
+        if (Comparison_Of_Arrays_2(tempW, posBlack)) DisplayWinPlayer("белые")
     }
     
-    // проверяем можна ли сделать ход
+    // Display Winner Player and Delete Event click
+    function DisplayWinPlayer(win) {
+        info_game.innerHTML = "Победили " + win + "!";
+        info_game.classList.add('gameOver')
+        grid.removeEventListener('click', ClickChecker);
+    }
+
+    // Check Turn Validity
     function checkTurnValidity(row, col) {
         const [rowStart, colStart] = activeElementCoords; 
-        const validity = DIAGONAL_TURNS_POSSIBLE ?  Math.abs(colStart - col) <= 1 && Math.abs(rowStart - row) <= 1:  Math.abs(colStart - col) + Math.abs(rowStart - row) <= 1
+        const validity = DIAGONAL_MOVE_POSSIBLE ?  Math.abs(colStart - col) <= 1 && Math.abs(rowStart - row) <= 1:  Math.abs(colStart - col) + Math.abs(rowStart - row) <= 1
         if (!board[row][col]) return validity;
     }
+
+    // Comparison of 2 arrays
     function Comparison_Of_Arrays(arr1, arr2) {
         return arr1.length === arr2.length && arr1.every((_, i) => arr1[i] === arr2[i]);
     }
+
+    // Comparison of 2 two-dimensional arrays
     function Comparison_Of_Arrays_2(arr1, arr2) {
         return arr1.length === arr2.length && arr1.every((_, i) => Comparison_Of_Arrays(arr1[i], arr2[i]));
-    }
-
-    // выводим ошибку если елемент на елемент
-    function doSomeAction(row, col) {
-        const targetTeam = board[row][col]
-        console.log('Something just happened!');
-        console.log('activeTeam', activeTeam)
-        console.log('targetTeam', targetTeam)
     }
 });
